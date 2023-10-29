f80442319b24d306fa72360844b1958e3c77b947
Polawut K <66056057@kmitl.ac.th>
2023-10-29 11:38:30 +0700

    Add 3 Charts

--- app.py
+++ app.py
@@ -10,20 +10,26 @@ st.title('Layout and styling')
 st.write('''
 San Francisco Dataset !!!!
 ''')
-col1, col2, col3 = st.columns(3)
-with col1:
-     st.write('column 1')
-with col2:
-     st.write('column 2')
-with col3:
-     st.write('column 3')
 tree_df = pd.read_csv('trees.csv')
+col1, col2, col3 = st.columns(3)
 df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
 df_dbh_grouped.columns = ['tree_count']
-st.line_chart(df_dbh_grouped)
+
+with col1:
+     st.write('column 1')
+     st.line_chart(df_dbh_grouped)
+with col2:
+     st.write('column 2')
+     st.bar_chart(df_dbh_grouped)
+with col3:
+     st.write('column 3')
+     st.area_chart(df_dbh_grouped)
+
 st.caption('กราฟแสดงจํานวนต้นไม้ทั้งหมดซึ่งจัดกลุ่มตามเส้นผ่านศูนย์กลาง')
 st.title('แปรผล')
 st.write('''
 ส่วนในต้นไม้ใน San Fransisco มีเส้นผ่านศูนย์กลางขนาด 3 ฟุต
 ''')
+
+
 st.divider()
